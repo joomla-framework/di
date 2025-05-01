315acb2b7173c00a3d679812da5dd068bd1fc033
Fedik <getthesite@gmail.com>
2025-05-01 18:56:15 +0300

    lazy variants test

--- Tests/ContainerSetupTest.php
+++ Tests/ContainerSetupTest.php
@@ -335,4 +335,29 @@ class ContainerSetupTest extends TestCase
 
         $this->assertTrue($resource instanceof Stub6);
     }
+
+    /**
+     * @testdox  If the resource is created with a proxy class
+     *
+     * @uses     Joomla\DI\Container
+     */
+    public function testGetLazyProxyInstance()
+    {
+        if (PHP_VERSION_ID < 80400) {
+            $this->markTestSkipped('Lazy objects are only supported in PHP 8.4 or newer.');
+        }
+
+        $factoryCalled = false;
+
+        $container = new Container();
+        $container->lazy(Stub6::class, function () use (&$factoryCalled) {
+            $factoryCalled = true;
+            return new Stub6();
+        });
+
+        $resource = $container->get(Stub6::class);
+
+        $this->assertTrue((new \ReflectionClass(Stub6::class))->isUninitializedLazyObject($resource));
+        $this->assertFalse($factoryCalled);
+    }
 }
